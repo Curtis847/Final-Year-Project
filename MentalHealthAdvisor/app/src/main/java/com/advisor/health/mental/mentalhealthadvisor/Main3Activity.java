30e6147a0e56bef46f8ea55d5615e399064ae417
Curtis Murphy <murphy.curtis12@gmail.com>
2017-11-13 18:54:29 +0000

    Fourth Version with Radio Groups for SelfChecker Answers

--- MentalHealthAdvisor/app/src/main/java/com/advisor/health/mental/mentalhealthadvisor/Main3Activity.java
+++ MentalHealthAdvisor/app/src/main/java/com/advisor/health/mental/mentalhealthadvisor/Main3Activity.java
@@ -2,6 +2,8 @@ package com.advisor.health.mental.mentalhealthadvisor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+    /*final RadioButton yesBtn = (RadioButton)findViewById(R.id.qstnOneYes);
+    final RadioButton noBtn = (RadioButton)findViewById(R.id.qstnOneNo);
+    final RadioButton sometimesBtn = (RadioButton)findViewById(R.id.qstnOneSmetme);*/
+
+
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.qstnOneYes:
+                if (checked)
+                    break;
+
+            case R.id.qstnOneNo:
+                if (checked)
+                    break;
+
+            case R.id.qstnOneSmetme:
+                if(checked)
+                    break;
+        }
+        /*yesBtn.equals(10);
+        if (yesBtn.isChecked()) {
+
+
+        }*/
+
     }
 }
