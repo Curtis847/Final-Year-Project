4c17f04e940e41226fe0b664e684dd9ed6502b3b
Curtis847 <murphy.curtis12@gmail.com>
2018-03-26 13:28:40 +0100

    Fifth Version Got to create child in Firebase based on edit text entry. Entry not on screen

--- MentalHealthAdvisorDr/app/src/main/java/com/messenger/practive/mentalhealthadvisordr/MainActivity.java
+++ MentalHealthAdvisorDr/app/src/main/java/com/messenger/practive/mentalhealthadvisordr/MainActivity.java
@@ -29,47 +29,36 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.Registry;
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private EditText quizNameTxt;
     private Button mButton;
     private EditText editText;
     private DatabaseReference mDatabase;
     private RecyclerView quizList;
+    private Button saveNameBtn;
 
-    FirebaseStorage storage;
-    StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-        storage = FirebaseStorage.getInstance();
-        storageReference = storage.getReference();
-        mDatabase = FirebaseDatabase.getInstance().getReference().child(editText.getText().toString());
+        quizNameTxt = (EditText) findViewById(R.id.quizNameTxt);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Quiz");
         quizList = (RecyclerView) findViewById(R.id.quizRec);
         quizList.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -77,12 +66,40 @@ public class MainActivity extends AppCompatActivity {
         quizList.setLayoutManager(linearLayoutManager);
         mButton = (Button) findViewById(R.id.addBtn);
         editText = (EditText) findViewById(R.id.questionTxtField);
+        saveNameBtn = (Button) findViewById(R.id.saveNameBtn);
+
+        saveNameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatabase.push().setValue(quizNameTxt.getText().toString());
+            }
+        });
+
+       mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String questionValue = editText.getText().toString().trim();
+                if (!TextUtils.isEmpty(questionValue)) {
+                    final DatabaseReference newQuestion = mDatabase.push();
+                    newQuestion.child("Quiz").setValue(questionValue);
+                }
+
+            }
+        });
 
     }
 
+    /*public void submitChoice(View view) {
+        final String questionValue = editText.getText().toString().trim();
+        if (!TextUtils.isEmpty(questionValue)) {
+            final DatabaseReference newQuestion = mDatabase.push();
+            newQuestion.child("Question").setValue(questionValue);
+        }
+    }*/
+
     Query query = FirebaseDatabase.getInstance()
             .getReference()
-            .child(editText.getText().toString())
+            .child("Quiz")
             .limitToLast(50);
 
     FirebaseRecyclerOptions<Question> options =
@@ -95,14 +112,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public QuestionViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.activity_main, parent, false);
+                    .inflate(R.layout.question_layout, parent, false);
 
             return new QuestionViewHolder(view);
         }
         @Override
         protected void onBindViewHolder(QuestionViewHolder holder, int position, Question model) {
             holder.setQuestion(model.getQuestion());
-            //holder.setAnswerOne(model.getAnswerOne());
+            holder.setAnswerOne(model.getAnswerOne());
         }
     };
 
@@ -118,6 +135,25 @@ public class MainActivity extends AppCompatActivity {
 
 
     }*/
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FBRA.startListening();
+
+        quizList.setAdapter(FBRA);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        FBRA.stopListening();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 
 
     private static class QuestionViewHolder extends RecyclerView.ViewHolder {
@@ -131,9 +167,9 @@ public class MainActivity extends AppCompatActivity {
             TextView question_content = (TextView) mView.findViewById(R.id.questionTxt);
             question_content.setText(sQuestion);
         }
-        /*void setAnswerOne(String answerOne) {
+        void setAnswerOne(String answerOne) {
             RadioButton answerOne_content = (RadioButton) mView.findViewById(R.id.answerOne);
             answerOne_content.setText(answerOne);
-        }*/
+        }
     }
 }
--- MentalHealthAdvisorDr/app/src/main/java/com/messenger/practive/mentalhealthadvisordr/ProfileActivity.java
+++ MentalHealthAdvisorDr/app/src/main/java/com/messenger/practive/mentalhealthadvisordr/ProfileActivity.java
@@ -1,13 +1,27 @@
 package com.messenger.practive.mentalhealthadvisordr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private Button createQuizBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        createQuizBtn = (Button) findViewById(R.id.createQuizBtn);
+
+
+    }
+
+    public void createQuiz(View v) {
+        startActivity(new Intent(ProfileActivity.this, MainActivity.class));
     }
 }
